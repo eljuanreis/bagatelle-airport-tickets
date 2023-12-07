7104ce09effdc4c1e1f544aaa115520ae1c50b3f
Juan Reis <contatojuanzito@gmail.com>
2023-12-07 17:29:11 -0300

    add menu

--- src/application/Index.java
+++ src/application/Index.java
@@ -1,16 +1,7 @@
 package application;
 
-import dao.AirlineDAO;
-import dao.FlightDAO;
-import dao.PassengerDAO;
-import dao.TicketDAO;
 import database.DBManager;
-import entity.Airline;
-import entity.Flight;
-import entity.Passenger;
-import entity.Ticket;
-import utils.ParseDate;
-import utils.ParseTime;
+import javafx.application.Application;
 
 public class Index {
 
@@ -21,59 +12,10 @@ public class Index {
 			DBManager db = DBManager.getInstance();
 			db.testConnection();
 			
-			Passenger p = new Passenger();
-			p.setId(1);
-			p.setName("aa");
-			p.setCpf("aaa");
-			p.setPhone("123");
-			p.setEmail("aasd");
-//			p.setBirthDate(ParseDate.toDatetime("01/01/2000"));
-//			
-////			
-//			PassengerDAO pd = new PassengerDAO(p);
-//			pd.create();
-//			pd.index();
-			
-			Airline a = new Airline();
-			a.setId(1);
-			a.setName("Gol");
-//			System.out.println(a.getName());
-//			AirlineDAO ad = new AirlineDAO(a);
-//			ad.create();
-//			ad.index();
-			
-			Flight f = new Flight();
-			f.setId(7);
-			f.setAirline(a);
-			f.setAirplane("Boeing 737");
-			f.setNumber("G70787");
-			f.setDate(ParseDate.toDatetime("06/12/2023"));
-			f.setBoardingTime(ParseTime.toLocalTime("18:00"));
-			f.setDepartureTime(ParseTime.toLocalTime("19:00"));
-			f.setArrivalTime(ParseTime.toLocalTime("20:00"));
-			f.setDepartureAirport("SBGR");
-			f.setDestinationAirport("SBRJ");
-			f.setGate("1A");
-//			
-//			FlightDAO fd = new FlightDAO(f);
-//			fd.create();
-//			fd.index();
-			
-			Ticket t = new Ticket();
-			t.setFlight(f);
-			t.setPassenger(p);
-			t.setSeat("33A");
-			t.setSeatClass("Economy Class");
-			
-			TicketDAO td = new TicketDAO(t);
-			td.create();
-			td.index();
-			
+			Application.launch(boundary.Index.class, args);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		
-		// Lançar telas da aplicação
+		}		
 	}
 }
--- src/boundary/Index.java
+++ src/boundary/Index.java
@@ -1,5 +1,73 @@
 package boundary;
 
-public class Index {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class Index extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(Index.class, args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		BorderPane mainPane = new BorderPane();
+		Scene scn = new Scene(mainPane, 500, 300);
+		stage.setResizable(false);
+		GridPane paneForm = new GridPane();
+		GridPane paneTop = new GridPane();
+
+		Label text = new Label("Selecione uma opção:");
+		
+		Button airline = new Button("Cadastrar companhias");
+		Button passenger = new Button("Cadastrar passageiro");
+		Button flight = new Button("Cadastrar vôos");
+		Button ticket = new Button("Cadastrar tickets");
+		
+		paneTop.add(text, 0, 0);
+		
+		paneForm.add(airline, 0, 1);
+		paneForm.add(passenger, 0, 2);
+		paneForm.add(flight, 0, 3);
+		paneForm.add(ticket, 0, 4);
+		
+		mainPane.setTop(paneTop);
+		mainPane.setCenter(paneForm);
+		
+		mainPane.setStyle("-fx-padding: 25px");
+		paneForm.setVgap(20);
+		
+		airline.setOnAction(x -> {
+			AirlineBoundary bd = new AirlineBoundary();
+			Stage bdStage = new Stage();
+			bd.start(bdStage);
+		});
+		
+		passenger.setOnAction(x -> {
+			PassengerBoundary bd = new PassengerBoundary();
+			Stage bdStage = new Stage();
+			bd.start(bdStage);
+		});
+		
+		flight.setOnAction(x -> {
+			FlightBoundary bd = new FlightBoundary();
+			Stage bdStage = new Stage();
+			bd.start(bdStage);
+		});
+		
+		ticket.setOnAction(x -> {
+			TicketBoundary bd = new TicketBoundary();
+			Stage bdStage = new Stage();
+			bd.start(bdStage);
+		});
+		
+		stage.setScene(scn);
+		stage.setTitle("Menu de opções");
+		stage.show();
+	}
 }
